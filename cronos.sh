cda79c5df812c78591f4227f40ec0d648563fddd
RafitaDev <rafaelcazari2013@gmail.com>
2026-04-26 20:33:55 -0300

    kernel: full performance pass (KernelSU + BPF JIT fixes + cgroup/zram tuning) perf(kernel): major optimization drop (KSU, BPF, scheduler, memory) kernel: stabilize + boost (KSU integration, BPF fixes, system tuning)
    this its not the commit with all this things, the commit with all of this things are on there    https://github.com/rafitadev/RafitaChan_8890/commit/24c2114640d194c47c19c6dabfcc405cdbbe7f05

--- cronos.sh
+++ cronos.sh
@@ -38,8 +38,8 @@ CR_KERNEL=$CR_DIR/arch/arm64/boot/Image
 # Compiled dtb by dtbtool
 CR_DTB=$CR_DIR/arch/arm64/boot/dtb.img
 # Kernel Name and Version
-CR_VERSION=V1.0
-CR_NAME=HeroGraceKernel
+CR_VERSION=V11.1.1
+CR_NAME=RafitaChan
 # Thread count
 CR_JOBS=$(nproc --all)
 # Target Android version
